9dafeb1a5dd6f70e87c454e0c874b6d996894a87
DerialE <DerialE@users.noreply.github.com>
2019-08-19 18:29:36 +0300

    1.6.0
    Glad

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,10 +1,9 @@
 #include "hzpch.h"
 #include "Application.h"
 
-#include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/Log.h"
 
-#include "GLFW/glfw3.h"
+#include <glad/glad.h>
 
 namespace Hazel
 {
